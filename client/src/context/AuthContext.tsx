bcf5f14c8f868272a8a99e31372ca51027f4abf4
Johnny Le <jwonnyleaf@gmail.com>
2025-01-25 16:46:03 -0600

    feat: protectedroute implemented

--- client/src/context/AuthContext.tsx
+++ client/src/context/AuthContext.tsx
@@ -1,7 +1,42 @@
-import { useState } from "react";
+import { User, AuthContextType } from '../types';
+import { createContext, useContext, useState, ReactNode } from 'react';
 
-export const AuthProvider: React.FC = ({ children: ReactNode }) => {
-    const [user, setUser] = useState<null>(null);
-    
-}
+const defaultAuthContext = {
+  user: null,
+  isAuthenticated: false,
+  login: () => {},
+  logout: () => {},
+};
 
+const AuthContext = createContext<AuthContextType>(defaultAuthContext);
+
+export const AuthProvider: React.FC<{ children: ReactNode }> = ({
+  children,
+}) => {
+  const [user, setUser] = useState<User | null>(null);
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+
+  const login = (user: User) => {
+    setUser(user);
+    setIsAuthenticated(true);
+  };
+
+  const logout = () => {
+    setUser(null);
+    setIsAuthenticated(false);
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, isAuthenticated, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error('useAuth must be used within an AuthProvider');
+  }
+  return context;
+};
---
+++ client/src/types/contextTypes.ts
@@ -0,0 +1,8 @@
+import { User } from './authTypes';
+
+export interface AuthContextType {
+  user: User | null;
+  isAuthenticated: boolean;
+  login: (user: User) => void;
+  logout: () => void;
+}
