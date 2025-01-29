a5483ebb221f4a5955542671c088f161c534fd5e
Johnny Le <jwonnyleaf@gmail.com>
2025-01-29 15:04:45 -0600

    fix: login image darkness

--- client/src/pages/Login/Login.tsx
+++ client/src/pages/Login/Login.tsx
@@ -25,7 +25,7 @@ export default function Login() {
         <img
           src={templogin}
           alt="Image"
-          className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+          className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.3] dark:grayscale"
         />
       </div>
     </div>
